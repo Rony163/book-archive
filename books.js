c4abadbb0a817736ce59d6364ac52a903411cd9a
Rony163 <minhajulislamrony43@gmail.com>
2021-09-02 00:47:33 +0600

    inner html add

--- books.js
+++ books.js
@@ -6,5 +6,18 @@ function searchBooks() {
 
     fetch(`http://openlibrary.org/search.json?q=${searchText}`)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayBook(data.docs));
+}
+
+const displayBook = booksInfo => {
+    const container = document.getElementById('result');
+    booksInfo.forEach(bookInfo => {
+        console.log(bookInfo);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h2>Book Name: ${bookInfo.title}</h2>
+        <h3>Author Name: ${bookInfo.author_name[0]} </h3>
+        `;
+        container.appendChild(div);
+    })
 }
